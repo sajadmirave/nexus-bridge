027bc220c7cccc70a27efe7f97f289c29e5ea13d
sajjad-mirave <banditofsocorstars@gmail.com>
2023-09-21 13:23:45 +0330

    delete test

--- test/se.js
+++
@@ -1,9 +0,0 @@
-function myRoute(app) {
-    app.use((req, res, next) => {
-        res.cookie('animal1', 'catsdadadsa', { httpOnly: true })
-        res.cookie('animal2', 'moosh', { httpOnly: true })
-        next()
-    });
-}
-
-module.exports = { myRoute };
